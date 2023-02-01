d0aa2bdb0a6454338213df6f06ee968414165687
spoddub <spoddub98@gmail.com>
2023-02-01 04:28:24 +0300

    created brain-prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python
+
+from brain_games.games import prime_game
+from brain_games.logic import launch
+
+
+def main():
+    launch(prime_game)
+
+
+if __name__ == '__main__':
+    main()
