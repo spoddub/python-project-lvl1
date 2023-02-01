7dbb85b778a8259e67f580dd85983711f9c82e51
spoddub <spoddub98@gmail.com>
2023-02-01 04:17:59 +0300

    created brain-progression

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python
+
+from brain_games.games import progression_game
+from brain_games.logic import launch
+
+
+def main():
+    launch(progression_game)
+
+
+if __name__ == '__main__':
+    main()
