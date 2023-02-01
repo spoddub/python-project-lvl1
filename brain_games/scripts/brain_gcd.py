f7c337fc644b4f2d23534b932f388177d8dbae89
spoddub <spoddub98@gmail.com>
2023-02-01 03:57:58 +0300

    created brian_gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python
+
+from brain_games.games import gcd_game
+from brain_games.logic import launch
+
+
+def main():
+    launch(gcd_game)
+
+
+if __name__ == '__main__':
+    main()
